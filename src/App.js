3fba6567b2cacb5624293f260f82277ac8c124f2
FilipKupanovac <80569573+FilipKupanovac@users.noreply.github.com>
2021-04-02 14:37:08 +0200

    Scroll functionality
    Added Scroll component and SEGA font for title

--- src/App.js
+++ src/App.js
@@ -1,35 +1,54 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import './App.css'
 
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots : robots,
+            robots : [],
             searchfield : ''
         }
+    } 
+    /*smart component: it's called like that
+     because it is class containing state*/
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users}))
     }
 
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
     }
-
+    /*
+    EVERY TIME STATE GETS UPDATED, FILE RE-RENDERS AGAIN
+    you can try it with console.log() for each constructor, 
+    componentDidMount and render functions
+    */
     render(){
         const filteredRobots = this.state.robots.filter(
             robots => {
                 return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
             }
             )
-        return(
-            <div className='tc'>
-                <h1>Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots = {filteredRobots}/>
-            </div>
-        );
+        if(this.state.robots.length === 0){
+            return <h1 className='tc'>Loading</h1>
+        } else{
+            return(
+                <div className='tc'>
+                    <h1 className='f1'>Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots = {filteredRobots}/>
+                    </Scroll>
+                </div>
+            );
+        }
     }
 }
 
